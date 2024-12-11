a6e6314a7dcfe8bdb8bce2b41564e12e5439e8ba
Become-devops <masterdevopsin2023@gmail.com>
2024-12-11 09:52:06 +0530

    c1

---
+++ Test.java
@@ -0,0 +1,7 @@
+class Test  // Defines a class named "Test"
+{
+    public static void main(String[] args)  // The main method is the entry point for the program
+    {
+        System.out.println("My First Java Program.");  // Prints the text to the console
+    }
+};
